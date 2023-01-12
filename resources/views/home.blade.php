a8bbb5a4665ef7d743d0630af51dc177d1158014
Komarru Jaman <61224778+Komarrujaman@users.noreply.github.com>
2023-01-12 21:43:50 +0700

    edit halaman dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,10 +3,6 @@
 <!-- Begin Page Content -->
 <div class="container-fluid">
 
-    <!-- Page Heading -->
-    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h1 mb-0 text-gray-800">Dashboard</h1>
-    </div>
 
     <!-- Content Row -->
     <div class="row">
@@ -19,98 +15,32 @@
         $lastUpdateTime->setTimeZone(new DateTimeZone('Asia/Jakarta'));
         $diff = $currentTime->diff($lastUpdateTime);
         ?>
-
-        @if ($item->alert == 1) <audio src="{{asset('assets/audio/1.wav')}}" autoplay></audio>
-        @elseif ($item->alert == 2) <audio src="{{asset('assets/audio/2.wav')}}" autoplay></audio>
-        @elseif ($item->alert == 3)<audio src="{{asset('assets/audio/3.wav')}}" autoplay></audio>
-        @endif
         <div class="col-xl-3 col-md-6 mb-2">
-            <div class="card shadow h-80 py-1 @if ($diff->d > 0 || $diff->h > 1) {{ 'border-secondary' }} 
-            @elseif ($item->alert == 1) {{ 'border-success'}}
-            @elseif ($item->alert == 2) {{ 'border-warning' }}
-            @elseif ($item->alert == 3) {{'border-danger' }}
-            @elseif ($item->alert == 0) {{'border-primary' }}
-            @endif" id="listDevice">
+            <div class="card shadow py-1" id="listDevice">
                 <div class="card-body">
                     <a href="{{url ('/info/'.$item->deviceName)}}" class="stretched-link"></a>
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="row">
-                                <div class="col-sm-8">
-                                    <div class="h6 font-weight-bold text-gray-800 text-uppercase">{{$item->sensorName}}</div>
-                                </div>
-                                <div class="col-4 text-right">
-                                    <i class="fas fa-solid fa-bell fa-lg @if ($diff->d > 0 || $diff->h > 1) {{ 'text-secondary' }} 
-            @elseif ($item->alert == 1) {{ 'text-success'}}
-            @elseif ($item->alert == 2) {{ 'text-warning' }}
-            @elseif ($item->alert == 3) {{'text-danger' }}
-            @elseif ($item->alert == 0) {{'text-primary' }}
-            @endif"></i>
+                                <div class="col-sm-12">
+                                    <div class="h5 font-weight-bold text-gray-800 text-uppercase mb-3">{{$item->sensorName}}</div>
                                 </div>
                             </div>
-                            <div class="h2 mb-0 font-weight-bold text-gray-800 mb-2">{{$item->level}} cm</div>
-                            <div class="table-responsive" style="font-size: 10px;">
-                                <table class="table table-borderless table-sm">
-
-                                    <tr>
-                                        <th class="font-weight-bold text-black text-uppercase">
-                                            RSSI
-                                        </th>
-                                        <td>
-                                            :
-                                        </td>
-                                        <td>
-                                            {{$item->rssi}}
-                                        </td>
-                                    </tr>
-
-                                    <tr>
-                                        <th class="font-weight-bold text-black text-uppercase">
-                                            SNR
-                                        </th>
-                                        <td>
-                                            :
-                                        </td>
-                                        <td>
-                                            {{$item->snr}}
-                                        </td>
-                                    </tr>
-
-                                    <tr>
-                                        <th class="font-weight-bold text-black text-uppercase">
-                                            Battery
-                                        </th>
-                                        <td>
-                                            :
-                                        </td>
-                                        <td>
-                                            {{$item->battery}}
-                                        </td>
-                                    </tr>
-
-                                    <tr>
-                                        <th class="font-weight-bold text-black text-uppercase">
-                                            Last Update
-                                        </th>
-                                        <td>
-                                            :
-                                        </td>
-                                        <td>
-                                            {{$item->lastUpdate}}
-                                        </td>
-                                    </tr>
-                                </table>
-
+                            <div class="row mb-2">
+                                <div class="h6 col-6 mb-0 font-weight-bold text-gray-800 mb-2"><i class="fas fa-solid fa-temperature-three-quarters fa-2x text-primary"></i> {{$item->temperature}} °C</div>
+                                <div class="h6 mb-0 col-6 font-weight-bold text-gray-800 mb-2"><i class="fas fa-solid fa-droplet fa-2x  text-primary"></i> {{$item->humidity}}%</div>
                             </div>
-
+                            <div class="row">
+                                <div class="h6 col-7 mb-0 font-weight-bold text-gray-800 mb-2"><i class="fas fa-solid fa-gauge-high fa-1x  text-primary"></i> {{$item->pressure}}hPa</div>
+                                <div class="h6 col-5 mb-0 font-weight-bold text-gray-800 mb-2"><i class="fas fa-solid fa-signal text-primary fa-1x"></i> {{$item->rssi}}</div>
+                            </div>
+                            <div class="mb-0 font-weight-bold text-gray-800 mt-3" style="font-size: 10px;">{{$item->lastUpdate}}</div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
-        @endforeach
-
-
     </div>
+    @endforeach
+
     @endsection
