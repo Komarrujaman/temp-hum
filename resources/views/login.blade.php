e9a7f39752b7bd0ff47dd0324f36abe156576052
Komarru Jaman <61224778+Komarrujaman@users.noreply.github.com>
2023-01-13 13:11:07 +0700

    logo di login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -24,28 +24,28 @@
     <div class="container">
         <!-- Outer Row -->
         <div class="row justify-content-center">
-            <div class="card o-hidden border-0 shadow-lg my-5" style="margin-top: 50%;">
+            <div class="card o-hidden border-0 shadow-lg my-2" style="margin-top: 50%;">
                 <div class="card-body p-0">
                     <!-- Nested Row within Card Body -->
-                    <div class="">
-                        <div class="p-5">
-                            <div class="text-center">
-                                <h1 class="h4 text-gray-900 mb-4">Selamat Datang Kembali</h1>
-                            </div>
-                            <form class="user" method="POST" action="{{route ('login.cek')}}">
-                                @csrf
-                                <div class="form-group">
-                                    <input type="text" class="form-control form-control-user" id="email" aria-describedby="emailHelp" name="username" placeholder="Username" required>
-                                </div>
-                                <div class="form-group">
-                                    <input type="text" class="form-control form-control-user" id="password" placeholder="Password" name="password" required>
-                                </div>
-                                <button type="submit" class="btn btn-primary btn-user btn-block">
-                                    Login
-                                </button>
-                                @include('sweetalert::alert')
-                            </form>
+
+                    <div class="p-5">
+                        <div class="text-center">
+                            <img class="mb-3" src="{{asset ('assets/img/logo-antares.png')}}" alt="antares-logo" width="300px">
+                            <h1 class="h4 text-gray-900 mb-4">Selamat Datang Kembali</h1>
                         </div>
+                        <form class="user" method="POST" action="{{route ('login.cek')}}">
+                            @csrf
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-user" id="email" aria-describedby="emailHelp" name="username" placeholder="Username" required>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-user" id="password" placeholder="Password" name="password" required>
+                            </div>
+                            <button type="submit" class="btn btn-primary btn-user btn-block">
+                                Login
+                            </button>
+                            @include('sweetalert::alert')
+                        </form>
                     </div>
                 </div>
             </div>
